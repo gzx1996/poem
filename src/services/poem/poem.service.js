e3a2e5cd0ed88c7313c81392b98dd89a46340b40
高振翔_time <xiangz.gao@timemicro.tech>
2021-02-02 17:07:38 +0800

    up

--- src/services/poem/poem.service.js
+++ src/services/poem/poem.service.js
@@ -11,10 +11,10 @@ module.exports = function (app) {
   };
 
   // Initialize our service with any options it requires
-  app.use('/api/poem', new Poem(options, app));
+  app.use('/poem', new Poem(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('api/poem');
+  const service = app.service('poem');
 
   service.hooks(hooks);
 
